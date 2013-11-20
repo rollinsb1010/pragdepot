8aaff562bad748761d442a5f79ebb09ae0f13569
Brian Rollins <rollinsb@austin.rr.com>
2013-11-20 15:32:12 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pragdepot::Application.config.secret_key_base = '5cc70d72647130ce517b2e4a0f871b9b450c4a22a81fc6ea39d7dcd74b1027ef6f73e7942adfe5174b0d40f348ae4dfdc55cb1cacc6a509d4c2edf57fac6c438'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pragdepot::Application.config.session_store :cookie_store, key: '_pragdepot_session'
