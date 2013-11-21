e4084e52c0404ee67050503f7dd465e78c856269
Brian Rollins <rollinsb@austin.rr.com>
2013-11-21 01:00:37 -0600

    fixup

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -62,4 +64,26 @@ class ProductTest < ActiveSupport::TestCase
     end
 
   end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
+  end
+
+  # test "product is not valid without a unique title - i18n" do
+  #   product = Product.new(title: products(:ruby).title,
+  #                         description: "yyy",
+  #                         price: 1,
+  #                         image_url: "fred.gif")
+
+  #   assert !product.save
+  #   assert_equal I18n.translate('activerecord.errors.messages.taken'),
+  #     product.errors[:title].join('; ')
+  # end
+
 end
